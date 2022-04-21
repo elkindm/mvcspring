c0c1539551a57a9f332b5f759a25ce317649a552
Elkin Duran <44894517+elkindm@users.noreply.github.com>
2022-04-21 17:16:54 -0500

    se crea controlador y se agrega la libreria para el manejo del log, tambien se configura el log de la aplicación y se cambia el puerto

---
+++ mvcspringboot/src/main/java/co/com/libisoft/mvcspringboot/controllers/InicioController.java
@@ -0,0 +1,26 @@
+package co.com.libisoft.mvcspringboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+/**
+ * 
+ * @author elkin
+ *
+ */
+
+@RestController
+@Slf4j
+public class InicioController {
+
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(InicioController.class);
+	
+	@GetMapping("/")
+	public String inicio() {
+		log.info("message");
+		log.debug("prueba");
+		return "Hola mundo  mvc Spring";
+	}
+}
