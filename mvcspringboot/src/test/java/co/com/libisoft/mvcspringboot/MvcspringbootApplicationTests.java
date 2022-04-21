1d36d530449b1af318726837c0df886898ffa6ea
Elkin Duran <44894517+elkindm@users.noreply.github.com>
2022-04-21 16:58:14 -0500

    projecto mvc inicial

---
+++ mvcspringboot/src/test/java/co/com/libisoft/mvcspringboot/MvcspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.libisoft.mvcspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
