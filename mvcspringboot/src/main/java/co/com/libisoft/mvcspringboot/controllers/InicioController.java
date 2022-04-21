8cd324b757dcc92b0b80eb25f37314a680e3562b
Elkin Duran <44894517+elkindm@users.noreply.github.com>
2022-04-21 17:40:34 -0500

    se limpia codigo innecesario

--- mvcspringboot/src/main/java/co/com/libisoft/mvcspringboot/controllers/InicioController.java
+++ mvcspringboot/src/main/java/co/com/libisoft/mvcspringboot/controllers/InicioController.java
@@ -3,7 +3,6 @@ package co.com.libisoft.mvcspringboot.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 /**
  * 
