d881ddfa3a294e93ae1b31ef68ccdd83baac7048
Elkin Duran <44894517+elkindm@users.noreply.github.com>
2022-04-22 18:06:31 -0500

    clase tres: se pasa datos desde el controlador a la vista y tambien datos del properties

--- mvcspringboot/src/main/java/co/com/libisoft/mvcspringboot/controllers/InicioController.java
+++ mvcspringboot/src/main/java/co/com/libisoft/mvcspringboot/controllers/InicioController.java
@@ -1,6 +1,8 @@
 package co.com.libisoft.mvcspringboot.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,11 +16,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class InicioController {
 
+	@Value("${index.saludo}")
+	private String saludo;
 	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(InicioController.class);
 	
 	@GetMapping("/")
-	public String inicio() {
-		log.info("message");
+	public String inicio(Model model) {
+		log.info("message "+saludo);
+		model.addAttribute("saludo", saludo);
 		return "index";
 	}
 }
