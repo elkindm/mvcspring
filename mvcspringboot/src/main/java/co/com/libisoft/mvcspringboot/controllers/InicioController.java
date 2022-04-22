12a1d3eb22dfc2236908972222d0fad86ca2470a
Elkin Duran <44894517+elkindm@users.noreply.github.com>
2022-04-21 22:14:31 -0500

    se agregan mas propiedades al properties y html con thymeleaf y se modifica el controller

--- mvcspringboot/src/main/java/co/com/libisoft/mvcspringboot/controllers/InicioController.java
+++ mvcspringboot/src/main/java/co/com/libisoft/mvcspringboot/controllers/InicioController.java
@@ -1,7 +1,7 @@
 package co.com.libisoft.mvcspringboot.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 /**
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
  *
  */
 
-@RestController
+@Controller
 @Slf4j
 public class InicioController {
 
@@ -19,7 +19,6 @@ public class InicioController {
 	@GetMapping("/")
 	public String inicio() {
 		log.info("message");
-		log.debug("prueba");
-		return "Hola mundo  mvc Spring";
+		return "index";
 	}
 }
