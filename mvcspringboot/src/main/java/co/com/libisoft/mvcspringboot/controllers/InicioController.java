fe7159cec4602f503ba55814fb509928f0f8ffcc
Elkin Duran <44894517+elkindm@users.noreply.github.com>
2022-04-22 22:29:34 -0500

    clase 5 listar objectos y validarlos en la vista

--- mvcspringboot/src/main/java/co/com/libisoft/mvcspringboot/controllers/InicioController.java
+++ mvcspringboot/src/main/java/co/com/libisoft/mvcspringboot/controllers/InicioController.java
@@ -1,12 +1,13 @@
 package co.com.libisoft.mvcspringboot.controllers;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import co.com.libisoft.mvcspringboot.domain.Persona;
-import lombok.extern.slf4j.Slf4j;
 /**
  * 
  * @author elkin
@@ -14,7 +15,6 @@ import lombok.extern.slf4j.Slf4j;
  */
 
 @Controller
-@Slf4j
 public class InicioController {
 
 	@Value("${index.saludo}")
@@ -29,8 +29,22 @@ public class InicioController {
 		persona.setApellido("Duran");
 		persona.setEmail("gerenacia@libisoft.com.co");
 		persona.setTelefono("3214170011");
+		
+		var persona2=new Persona();
+		persona2.setNombre("libi");
+		persona2.setApellido("Duran");
+		persona2.setEmail("gerenacia@libisoft.com.co");
+		persona2.setTelefono("3214170011");
+		
+//		List<Persona> personas= new ArrayList();
+//		personas.add(persona);
+//		personas.add(persona2);
+		
+		var personas= Arrays.asList(persona,persona2);
+		
 		model.addAttribute("saludo", saludo);
 		model.addAttribute("cliente", persona);
+		model.addAttribute("clientes", personas);
 		return "index";
 	}
 }
