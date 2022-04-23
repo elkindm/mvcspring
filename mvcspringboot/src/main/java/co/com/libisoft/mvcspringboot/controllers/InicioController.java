282de3da6014733c5d099ecf0338c2966c7cdb80
Elkin Duran <44894517+elkindm@users.noreply.github.com>
2022-04-22 23:31:43 -0500

    Se configura la base de datos y se descarga las dependencias jpa y postgresql

--- mvcspringboot/src/main/java/co/com/libisoft/mvcspringboot/controllers/InicioController.java
+++ mvcspringboot/src/main/java/co/com/libisoft/mvcspringboot/controllers/InicioController.java
@@ -17,34 +17,15 @@ import co.com.libisoft.mvcspringboot.domain.Persona;
 @Controller
 public class InicioController {
 
-	@Value("${index.saludo}")
-	private String saludo;
+	
 	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(InicioController.class);
 	
 	@GetMapping("/")
 	public String inicio(Model model) {
-		log.info("message "+saludo);
-		var persona=new Persona();
-		persona.setNombre("elkin");
-		persona.setApellido("Duran");
-		persona.setEmail("gerenacia@libisoft.com.co");
-		persona.setTelefono("3214170011");
 		
-		var persona2=new Persona();
-		persona2.setNombre("libi");
-		persona2.setApellido("Duran");
-		persona2.setEmail("gerenacia@libisoft.com.co");
-		persona2.setTelefono("3214170011");
+		log.info("controller iniciao index");
 		
-//		List<Persona> personas= new ArrayList();
-//		personas.add(persona);
-//		personas.add(persona2);
-		
-		var personas= Arrays.asList(persona,persona2);
-		
-		model.addAttribute("saludo", saludo);
-		model.addAttribute("cliente", persona);
-		model.addAttribute("clientes", personas);
+//		model.addAttribute("clientes", personas);
 		return "index";
 	}
 }
